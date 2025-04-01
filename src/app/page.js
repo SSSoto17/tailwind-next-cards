f41ff58928363dc99717fd39f163dc91ab41102b
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-04-01 10:45:57 +0200

    cards dooone

--- src/app/page.js
+++ src/app/page.js
@@ -20,9 +20,9 @@ export default function Home() {
     },
   ];
   return (
-    <main>
+    <main className="h-full py-12">
       <ThemeToggle />
-      <ul>
+      <ul className="bg-white dark:bg-slate-800 dark:text-slate-50 rounded-xl p-6 max-w-[400px] m-auto">
         {data.map((item, i) => (
           <Card key={i} {...item} />
         ))}
@@ -33,18 +33,21 @@ export default function Home() {
 
 function Card({ title, category }) {
   return (
-    <li>
-      <a href="#0">
+    <li className="hover:bg-slate-100 dark:hover:bg-slate-700 rounded-lg p-4 group">
+      <a href="#0" className="flex gap-4 items-center">
         <div className="aspect-square w-10 rounded-md bg-[#cbd5e1]"></div>
-        <div>
-          <p>{title}</p>
-          <small>{category}</small>
+        <div className="flex flex-col justify-between">
+          <p className="font-bold">{title}</p>
+          <small className="text-slate-500 dark:text-slate-400">
+            {category}
+          </small>
         </div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="32"
           height="32"
           viewBox="0 0 24 24"
+          className="text-purple-600 dark:text-white opacity-0 group-hover:opacity-100 ml-auto transition-all duration-200 group-hover:translate-x-4"
         >
           <path
             fill="currentColor"
